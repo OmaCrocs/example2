779f2262f2227d428f7ae00b6406001a586714d4
OmaCrocs <mirko.ungerank@snets.it>
2022-05-21 09:35:22 +0200

    16.05.22
    MUSIC, STARTSCREEN, MUTE BUTTON

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 
 #define WIDTH 900
 #define HEIGHT 900
+#define NUM_FRAMES 1
 
 //Predeclaration
 void startscreen();
@@ -21,6 +22,9 @@ int main() {
     //TODO: MAP CHOOSE!
     //TODO: ANIMATION BEFORE CLOSING STARTSCREEN!
     //TOFIX: OTHER FONT/DESIGN ON STARTSCREEN!
+    //ME MINIMALZIEL: 1 MAP
+
+    //TODO: MUTE BUTTON WITH PIXELCREATOR
 
     startscreen();
 
@@ -32,14 +36,52 @@ void startscreen() {
     InitWindow(WIDTH, HEIGHT, "PacMan");
     SetTargetFPS(60);
 
+    //Buttons
+    Texture2D mute = LoadTexture("../assets/mute-mute.png");
+    Texture2D sound = LoadTexture("../assets/sound-button.png");
+    Texture2D pacman_start = LoadTexture("../assets/pacman-start.png");
+
+    // Define frame rectangle for drawing
+    float frameHeight = (float) mute.height / NUM_FRAMES;
+    Rectangle sourceRec = {0, 0, (float) mute.width, frameHeight};
+
+    Rectangle btnBounds = {750, 750, 100, 100};
+    int btnState = 0;
+    bool btnAction = false;
+    Vector2 mousePoint = {0.0f, 0.0f};
+
+
     //Theme Music
     InitAudioDevice();
     Music startmusic = LoadMusicStream("../assets/theme.mp3");
     PlayMusicStream(startmusic);
-
     while (!WindowShouldClose()) {
+        //Button
+        mousePoint = GetMousePosition();
+        btnAction = false;
+        if (CheckCollisionPointRec(mousePoint, btnBounds)) {
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+                btnState = 2;
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+                btnAction = true;
+            }
+        } else
+            btnState = 0;
+        sourceRec.y = btnState * frameHeight;
+
+
         BeginDrawing();
-        UpdateMusicStream(startmusic);   // Update music buffer with new stream data
+        DrawTexture(sound, 750, 750, WHITE);
+
+        if (btnAction == true) {
+            sound = LoadTexture("../assets/mute-button.png");
+            DrawTexture(sound, 750, 750, WHITE);
+            StopMusicStream(startmusic);
+        }
+        //TODO: START MUSIC STREAM AGAIN
+        UpdateMusicStream(startmusic);
+
+        //BUTTON
 
 
         //PACMAN TEXT
@@ -48,9 +90,8 @@ void startscreen() {
         DrawText("PACMAN", 235, 155, 100, YELLOW);
 
         //PACMAN FIGURE
-        Vector2 position = {450, 400};
-        DrawCircleSector(position, 100, 125, 415, 1, YELLOW);
-        DrawCircle(470, 340, 15, BLACK);
+        DrawTexture(pacman_start, 100, 50, YELLOW);
+
 
         DrawText("Press SPACE to play!", 140, 550, 60, RED);
         DrawText("Press SPACE to play!", 135, 555, 60, YELLOW);
@@ -66,9 +107,9 @@ void startscreen() {
             mapselect();
         }
     }
+
 }
 
 void mapselect() {
     printf("\n\nDONE\n\n");
-
 }
